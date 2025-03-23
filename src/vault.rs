d08e98c3f1202d27b79f6ab80993eb25f8d33be4
louib <code@louib.net>
2025-03-23 00:14:10 -0400

    chore: bump rand version

--- src/vault.rs
+++ src/vault.rs
@@ -74,7 +74,7 @@ impl Aegis {
 
     pub fn encrypt(&mut self, password: &str) -> Result<()> {
         // Create a new master key
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let mut master_key = [0u8; 32];
         rng.fill_bytes(&mut master_key);
 
@@ -407,7 +407,7 @@ impl HeaderSlot {
 
 impl Default for HeaderSlot {
     fn default() -> Self {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let mut salt = [0u8; 32];
         rng.fill_bytes(&mut salt);
 
@@ -435,7 +435,7 @@ pub struct HeaderParam {
 
 impl Default for HeaderParam {
     fn default() -> Self {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let mut nonce = [0u8; 12];
         rng.fill_bytes(&mut nonce);
 
